e68403ad91dc619ea89520195e92391d10858e94
Hai Pham <ngochai285nd@gmail.com>
2023-04-14 00:56:12 +0700

    update

---
+++ exercises/20230411/tcp_client.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define MAX_BUF_SIZE 1024
+
+int main(int argc, char *argv[])
+{
+    // Kiểm tra đầu vào
+    if (argc != 3)
+    {
+        printf("usage: %s <IP address> <Port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // Tạo socket
+    int client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (client == -1)
+    {
+        perror("socket() failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Thiết lập thông tin địa chỉ cho socket
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    server_addr.sin_port = htons(atoi(argv[2]));
+
+    // Kết nối đến server
+    if (connect(client, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
+        perror("connect() failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Connection to %s %s port [tcp/*] succeeded!\n", argv[1], argv[2]);
+
+    // Nhận dữ liệu từ bàn phím và gửi đến server
+    char buf[MAX_BUF_SIZE];
+    memset(buf, 0, MAX_BUF_SIZE);
+    while (1)
+    {
+        printf("Enter a message: ");
+        fgets(buf, MAX_BUF_SIZE, stdin);
+
+        // Nếu người dùng nhập q hoặc Q thì thoát
+        if (strcmp(buf, "q\n") == 0 || strcmp(buf, "Q\n") == 0)
+            break;
+
+        // Gửi tin nhắn đến server
+        if (send(client, buf, strlen(buf), 0) == -1)
+        {
+            perror("send() failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Đóng socket
+    close(client);
+    return 0;
+}
---
+++ exercises/20230411/tcp_server.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define MAX_CLIENT 5
+#define MAX_BUF_SIZE 1024
+
+int main(int argc, char *argv[])
+{
+    // Kiểm tra đầu vào
+    if (argc != 4)
+    {
+        printf("usage: %s <Port> <The file contains the greeting> <The file that stores the content the client sends to>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // Đọc nội dung tệp tin chứa câu chào
+    FILE *fp = fopen(argv[2], "r");
+    if (fp == NULL)
+    {
+        perror("fopen() failed");
+        exit(EXIT_FAILURE);
+    }
+    fseek(fp, 0, SEEK_END);
+    long fsize = ftell(fp);
+    rewind(fp);
+    char *greeting = (char *)malloc(fsize + 1);
+    if (greeting == NULL)
+    {
+        perror("malloc() failed");
+        exit(EXIT_FAILURE);
+    }
+    memset(greeting, 0, fsize + 1);
+    fread(greeting, fsize, 1, fp);
+    fclose(fp);
+
+    // Tạo socket
+    int server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (server == -1)
+    {
+        perror("socket() failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Thiết lập thông tin địa chỉ cho socket
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(atoi(argv[1]));
+
+    // Gán địa chỉ cho socket
+    if (bind(server, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
+        perror("bind() failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Lắng nghe kết nối từ client
+    if (listen(server, MAX_CLIENT) == -1)
+    {
+        perror("listen() failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Nhận dữ liệu từ client
+    char buf[MAX_BUF_SIZE];
+    memset(buf, 0, MAX_BUF_SIZE);
+    while (1)
+    {
+        printf("Waiting for client...\n");
+        
+        // Chấp nhận kết nối từ client
+        struct sockaddr_in client_addr;
+        memset(&client_addr, 0, sizeof(client_addr));
+        socklen_t client_addr_len = sizeof(client_addr);
+        int client = accept(server,
+                            (struct sockaddr *)&client_addr,
+                            &client_addr_len);
+        if (client == -1)
+        {
+            perror("accept() failed");
+            exit(EXIT_FAILURE);
+        }
+        printf("Accepted socket %d from IP: %s:%d\n",
+               client,
+               inet_ntoa(client_addr.sin_addr),
+               ntohs(client_addr.sin_port));
+
+        // Gửi câu chào đến client
+        if (send(client, greeting, strlen(greeting), 0) == -1)
+        {
+            perror("send() failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Nhận dữ liệu từ client và ghi vào tệp tin
+        if (recv(client, buf, MAX_BUF_SIZE, 0) == -1)
+        {
+            perror("recv() failed");
+            exit(EXIT_FAILURE);
+        }
+
+        fp = fopen(argv[3], "a");
+        if (fp == NULL)
+        {
+            perror("fopen() failed");
+            exit(EXIT_FAILURE);
+        }
+        fprintf(fp, "%s", buf);
+        fclose(fp);
+
+        // Đóng kết nối với client
+        close(client);
+    }
+    free(greeting);
+
+    // Đóng socket
+    close(server);
+    return 0;
+}
