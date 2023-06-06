ae2adafb088c1bec9a34bea354eaddedee7b3047
Hai Pham <ngochai285nd@gmail.com>
2023-06-06 20:51:36 +0700

    update time_server

--- exercises/20230606/time_server.c
+++ exercises/20230606/time_server.c
@@ -10,31 +10,10 @@
 #define BUF_SIZE 1024
 #define MAX_CLIENT 10
 
-void format_time(char buf[], const char *format)
-{
-    time_t now = time(NULL);
-    struct tm *timeinfo = localtime(&now);
-    if (strcmp(format, "dd/mm/yyyy") == 0)
-    {
-        strftime(buf, BUF_SIZE, "%d/%m/%Y\n", timeinfo);
-    }
-    else if (strcmp(format, "dd/mm/yy") == 0)
-    {
-        strftime(buf, BUF_SIZE, "%d/%m/%y\n", timeinfo);
-    }
-    else if (strcmp(format, "mm/dd/yyyy") == 0)
-    {
-        strftime(buf, BUF_SIZE, "%m/%d/%Y\n", timeinfo);
-    }
-    else if (strcmp(format, "mm/dd/yy") == 0)
-    {
-        strftime(buf, BUF_SIZE, "%m/%d/%y\n", timeinfo);
-    }
-    else
-    {
-        strcpy(buf, "Invalid format\n");
-    }
-}
+struct sockaddr_in client_addr;
+
+void format_time(char *, const char *);
+void *client_proc(void *);
 
 int main(int argc, char *argv[])
 {
@@ -81,7 +60,6 @@ int main(int argc, char *argv[])
                ntohs(server_addr.sin_port));
 
         // Chấp nhận kết nối
-        struct sockaddr_in client_addr;
         memset(&client_addr, 0, sizeof(client_addr));
         socklen_t client_addr_len = sizeof(client_addr);
         int client = accept(server, (struct sockaddr *)&client_addr, &client_addr_len);
@@ -94,102 +72,136 @@ int main(int argc, char *argv[])
                inet_ntoa(client_addr.sin_addr),
                ntohs(client_addr.sin_port));
 
-        // Tạo tiến trình con để xử lý client
-        if (fork() == 0)
+        // Tạo thread để xử lý client
+        pthread_t thread_id;
+        if (pthread_create(&thread_id, NULL, client_proc, &client) != 0)
         {
-            // Tiến trình con
-            // Đóng socket server
-            close(server);
-
-            // Xử lý client
-            while (1)
-            {
-                // Gửi yêu cầu đến client
-                char *msg = "Enter command: ";
-                if (send(client, msg, strlen(msg), 0) < 0)
-                {
-                    perror("send() failed");
-                    break;
-                }
-
-                // Nhận yêu cầu từ client
-                char buf[BUF_SIZE];
-                memset(buf, 0, BUF_SIZE);
-                int len = recv(client, buf, BUF_SIZE, 0);
-                if (len < 0)
-                {
-                    perror("recv() failed");
-                    break;
-                }
-                else if (len == 0)
-                {
-                    printf("Client from %s:%d disconnected\n",
-                           inet_ntoa(client_addr.sin_addr),
-                           ntohs(client_addr.sin_port));
-                    break;
-                }
-                else
-                {
-                    // Xoá ký tự xuống dòng
-                    buf[strcspn(buf, "\n")] = 0;
-
-                    // Thoát nếu client gửi exit hoặc quit
-                    if(strcmp(buf, "exit") == 0 || strcmp(buf, "quit") == 0) {
-                        printf("Client from %s:%d disconnected\n",
-                           inet_ntoa(client_addr.sin_addr),
-                           ntohs(client_addr.sin_port));
-                        break;
-                    }
-
-                    // Xử lý yêu cầu
-                    char cmd[BUF_SIZE];
-                    char format[BUF_SIZE];
-                    char temp[BUF_SIZE];
-                    int ret = sscanf(buf, "%s %s %s", cmd, format, temp);
-                    if (ret == 2)
-                    {
-                        if (strcmp(cmd, "GET_TIME") == 0)
-                        {
-                            memset(buf, 0, BUF_SIZE);
-                            format_time(buf, format);
-                            if (send(client, buf, strlen(buf), 0) < 0)
-                            {
-                                perror("send() failed");
-                                break;
-                            }
-                        } else {
-                            char *msg = "Invalid command\n";
-                            if (send(client, msg, strlen(msg), 0) < 0)
-                            {
-                                perror("send() failed");
-                                break;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        char *msg = "Invalid command\n";
-                        if (send(client, msg, strlen(msg), 0) < 0)
-                        {
-                            perror("send() failed");
-                            break;
-                        }
-                    }
-                }
-            }
-
-            // Đóng socket client
-            close(client);
-
-            // Kết thúc tiến trình con
-            exit(EXIT_SUCCESS);
+            perror("pthread_create() failed");
+            exit(EXIT_FAILURE);
         }
+        pthread_detach(thread_id);
 
-        close(client);
     }
 
     // Đóng socket
     close(server);
 
     return 0;
+}
+
+void format_time(char buf[], const char *format)
+{
+    time_t now = time(NULL);
+    struct tm *timeinfo = localtime(&now);
+    if (strcmp(format, "dd/mm/yyyy") == 0)
+    {
+        strftime(buf, BUF_SIZE, "%d/%m/%Y\n", timeinfo);
+    }
+    else if (strcmp(format, "dd/mm/yy") == 0)
+    {
+        strftime(buf, BUF_SIZE, "%d/%m/%y\n", timeinfo);
+    }
+    else if (strcmp(format, "mm/dd/yyyy") == 0)
+    {
+        strftime(buf, BUF_SIZE, "%m/%d/%Y\n", timeinfo);
+    }
+    else if (strcmp(format, "mm/dd/yy") == 0)
+    {
+        strftime(buf, BUF_SIZE, "%m/%d/%y\n", timeinfo);
+    }
+    else
+    {
+        strcpy(buf, "Invalid format\n");
+    }
+}
+
+void *client_proc(void *param)
+{
+    int client = *(int *)param;
+
+    // Xử lý client
+    while (1)
+    {
+        // Gửi yêu cầu đến client
+        char *msg = "Enter command: ";
+        if (send(client, msg, strlen(msg), 0) < 0)
+        {
+            perror("send() failed");
+            break;
+        }
+
+        // Nhận yêu cầu từ client
+        char buf[BUF_SIZE];
+        memset(buf, 0, BUF_SIZE);
+        int len = recv(client, buf, BUF_SIZE, 0);
+        if (len < 0)
+        {
+            perror("recv() failed");
+            break;
+        }
+        else if (len == 0)
+        {
+            printf("Client from %s:%d disconnected\n",
+                   inet_ntoa(client_addr.sin_addr),
+                   ntohs(client_addr.sin_port));
+            close(client);
+            break;
+        }
+        else
+        {
+            // Xoá ký tự xuống dòng
+            buf[strcspn(buf, "\n")] = 0;
+
+            // Thoát nếu client gửi exit hoặc quit
+            if (strcmp(buf, "exit") == 0 || strcmp(buf, "quit") == 0)
+            {
+                printf("Client from %s:%d disconnected\n",
+                       inet_ntoa(client_addr.sin_addr),
+                       ntohs(client_addr.sin_port));
+                close(client);
+                break;
+            }
+
+            // Xử lý yêu cầu
+            char cmd[BUF_SIZE];
+            char format[BUF_SIZE];
+            char temp[BUF_SIZE];
+            int ret = sscanf(buf, "%s %s %s", cmd, format, temp);
+            if (ret == 2)
+            {
+                if (strcmp(cmd, "GET_TIME") == 0)
+                {
+                    memset(buf, 0, BUF_SIZE);
+                    format_time(buf, format);
+                    if (send(client, buf, strlen(buf), 0) < 0)
+                    {
+                        perror("send() failed");
+                        break;
+                    }
+                }
+                else
+                {
+                    char *msg = "Invalid command\n";
+                    if (send(client, msg, strlen(msg), 0) < 0)
+                    {
+                        perror("send() failed");
+                        break;
+                    }
+                    continue;
+                }
+            }
+            else
+            {
+                char *msg = "Invalid command\n";
+                if (send(client, msg, strlen(msg), 0) < 0)
+                {
+                    perror("send() failed");
+                    break;
+                }
+                continue;
+            }
+        }
+    }
+
+    return NULL;
 }
